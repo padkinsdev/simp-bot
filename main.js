ecffa937a1c1c18aeb0fe690ddc0d428d42cf457
Kate Adkins <kateadkins@kates-mbp.myfiosgateway.com>
2020-11-19 14:30:56 -0500

    ahhhh

--- main.js
+++ main.js
@@ -4,15 +4,35 @@ const config = require('./config.json');
 var simpDelay = 3600000;
 const client = new discord.Client();
 const simpRecipients = [
-    "621357020808740924",
-    "293841274639745025",
-    "215249462665281536",
-    "359196366171537410",
-    "618320455287177241",
-    "204035386689716225",
-    "317377671598178304"
+    "621357020808740924", // Neptune
+    "293841274639745025", // Mercury/Deep
+    "215249462665281536", // Venus/Gert
+    "359196366171537410", // Pluto
+    "618320455287177241", // Saturn/Mich
+    "204035386689716225", // Artemis/Kate
+    "317377671598178304", // corot-7b
+    "544919325417537570", // Brit(ney)
+    "136265284695621632", // satsui no thotto
+    "758888232200568892", // LVIes/Ceres
+    "691353290558013530", // T
+    "223118324475625472", // HaydenGotBored
+    "143077292938100737", // Ligondy
+    "402615865620561920", // Wretched Brat
+    "360130321691443200", // CannibalKin
+    "249123434645159936", // Orion
+    "572283494193954817", // Jupiter/Nikkie/Nikhil
+    "486341657869156353", // Mars
+    "317459110218170369", // Nix
+    "219935116443516928", // Quasar
+    "755618328818614312", // Uranus
+    "324216151628447744", // Finnn
+    "763601080289591317" // Scorpius
 ];
-
+/*
+const affirmationLines = [
+    "You have a lot more control over your life than you might think",
+    "You are beautiful"
+]*/
 client.on('ready', () => {
     console.log("I'm alive!");
     setInterval(() => {
