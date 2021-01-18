03e1cdf8e8c759b405fbcc67bdc9b74df2ee967b
Kate Adkins <>
2021-01-18 01:15:00 -0500

    Hoo boy this is going to be hard

--- main.js
+++ main.js
@@ -313,7 +313,7 @@ function remindToDrinkWater() {
 exports.client = client;
 exports.logger = logger;
 exports.tasks = [
-    new Task("simp generator", 60000/*10800000*/, getRandomUserToSimp),
-    new Task("upload logs to cloud", 3600000, uploadLogsToCloud),
-    new Task("remind everyone to drink water", 120000, remindToDrinkWater)
+    //new Task("simp generator", 60000/*10800000*/, getRandomUserToSimp),
+    new Task("upload logs to cloud", 3600000, uploadLogsToCloud)//,
+    //new Task("remind everyone to drink water", 120000, remindToDrinkWater)
 ];
