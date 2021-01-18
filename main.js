a7041b7d378e010cd95466128b185f5e2338a420
Kate Adkins <>
2021-01-18 01:03:19 -0500

    Oh no! Now it doesn't work again!

--- main.js
+++ main.js
@@ -314,6 +314,6 @@ exports.client = client;
 exports.logger = logger;
 exports.tasks = [
     new Task("simp generator", 60000/*10800000*/, getRandomUserToSimp),
-    new Task("upload logs to cloud", 3600000, uploadLogsToCloud)/*,
-    new Task("remind everyone to drink water", 60000, remindToDrinkWater)*/
+    new Task("upload logs to cloud", 3600000, uploadLogsToCloud),
+    new Task("remind everyone to drink water", 120000, remindToDrinkWater)
 ];
